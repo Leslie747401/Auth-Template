ee7f7a9c982a93984719a955b49a3a395d52306f
Leslie747401 <leslie747401@gmail.com>
2024-12-30 22:56:06 +0530

    Resolved Deployment errors

--- app/login/GoogleSignin.tsx
+++ app/login/GoogleSignin.tsx
@@ -2,10 +2,8 @@
 
 import { useState } from "react";
 import { useSearchParams } from "next/navigation";
-
 import { createClient } from "@/utils/supabase/client";
 import { Button } from "@/components/ui/button";
-import { toast } from "@/hooks/use-toast";
 import { Loader2 } from "lucide-react";
 import Image from "next/image";
 
